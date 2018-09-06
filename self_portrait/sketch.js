fb2c275cac012c1284666308bf743095b41ce9cf
Jon Darling <jon.darling@stu.bmcc.cuny.edu>
2018-09-06 15:33:42 -0400

    self portrait

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -12,24 +12,33 @@ function draw() {
 	background("grey");
     
 	noStroke();
+    
    
      // nose
 	fill("green");
-    ellipse(317, 180,80, 100);
+    ellipse(317, 200,80, 100);
+     // mouth 2
+    noStroke()
+     arc(315, 300, 250, 200, 0, PI);
     // mouth
     stroke("black")
     strokeWeight(5)
     
+   
+    
+    
+    
     fill("red")
-      arc(315, 300, 250, 200, 0, PI);
-//    // mouth 2
-//    rect(150,280,100,25,10)
+      arc(300, 300, 250, 200, 0, PI);
 
     // eyes
     noStroke();
+    
     fill ("blue")
-    ellipse(500, 65, 80);
-    ellipse(135, 65, 80);
+    ellipse(500, 90, 80);
+    ellipse(135, 90, 80);
+    
+   
 	
 	
 	
@@ -41,13 +50,7 @@ function draw() {
     	
 	
 	
-//	//sockets
-//	fill("white");
-//    rect(30, 20, 200, 88);
-//    rect(400, 20, 200, 88);
-//    
-   
-	
+
     
 }
 
