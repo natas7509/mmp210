598705391d8aa462469010e74fe1f5a58ce4ce18
Jon Darling <jon.darling@stu.bmcc.cuny.edu>
2018-09-28 23:35:05 -0400

    updated instructions

--- selfportrait-2/sketch.js
+++ selfportrait-2/sketch.js
@@ -15,10 +15,17 @@ function setup() {
     textSize(20);
 }
 
+var s = 10;
+
 var eyeSize = 85;
+var mouthSize = 80;
 
 var leftEyeX1 = 175;
 var leftEyeY1 = 100;
+
+ 
+
+
 
 
 
@@ -35,13 +42,13 @@ var leftWhitesEyesX2 = 130;
 var rightWhitesEyesX1 = 170;
 var rightWhitesEyesY1 = 100;
 var rightWhitesEyesX2 = 130;  
-var eyebrowsleftX1 = 170;
+var eyebrowsleftX1 = 150;
 var eyebrowsleftY1 = 40;
-var eyebrowsleftX2 = 130;
+var eyebrowsleftX2 = 140;
 var eyebrowsleftY2 = 20;
 var eyebrowsRightX1 = 450;
 var eyebrowsRightY1 = 15;
-var eyebrowsRightX2 = 130;
+var eyebrowsRightX2 = 150;
 var eyebrowsRightY2 = 20;
 var pupilsRightX1 = 200;
 var pupilsRightY1 = 100;
@@ -64,6 +71,10 @@ var nostrilsX1 = 300;
 var nostrilsY1 = 235; 
 var nostrilsX2 = 20;
 var nostrilsY2 = 10; 
+var nostrilsX1r = 344; 
+var nostrilsY1r = 233; 
+var nostrilsX2r = 25;
+var nostrilsY2r = 10;
 
 var mouthShadowX1 = 315;
 var mouthShadowY1 = 300;
@@ -95,7 +106,8 @@ var bagsRightY3 = 150;
 
 
 function draw() {
-    
+ 
+   
       
     // Canvas
 	background("#F7F1DA");
@@ -103,31 +115,12 @@ function draw() {
     noStroke();
     
      var eyePos = map(mouseX,0,width,-20, 20);
-    
-    //     text(frameCount,40,40);
-    
-//    ellipse(frameCount, 100, 40);
-    
-   
-    
-    	
-    
-    
-	
-
-     
-   
-  
-    
-
-   
-    
     	
       
-   
-     
     // Facial lines
-    stroke (5)
+    
+    stroke("black")
+    strokeWeight(5)
     
     
 	line(40, 120, 40, 20); // two xy coordinates
@@ -142,11 +135,20 @@ function draw() {
     line(510, 400, 330,500); // two xy coordinates
     line(328, 500, 280,500); // two xy coordinates
     
+    // crows-feet-right
+    stroke ("#917A4F")
+    strokeWeight(2)
     
+    line(60, 110, 90,100); // two xy coordinates
+    line(60, 100, 90,100); // two xy coordinates
+    line(60, 90, 90,100); // two xy coordinates
+    // crows-feet-left
+    stroke ("#917A4F")
+    strokeWeight(2)
     
-     
-    
-    
+    line(590, 90, 560,100); // two xy coordinates
+    line(590, 100, 560,100); // two xy coordinates
+    line(590, 110, 560,100); // two xy coordinates
    
     
    
@@ -154,27 +156,47 @@ function draw() {
     
 	fill("#917A4F");
     noStroke()
-//    ellipse(317, 200,80, 120);
-    	triangle(noseX1, noseY1, noseX2, noseY2,noseX3, noseY3); // three xy coordinates
+    stroke ("#917A4F");
+    line(300, 120, 280, 180);
+    stroke ("#917A4F");
+    line(330, 120, 360, 180);
+   
+    noStroke()
+    triangle(noseX1, noseY1, noseX2, noseY2,noseX3, noseY3); // three xy coordinates
+   
+    	
     // nostrils
     fill ("black")
     ellipse (nostrilsX1, nostrilsY1, nostrilsX2, nostrilsY2);
+    ellipse (nostrilsX1r, nostrilsY1r, nostrilsX2r, nostrilsY2r);
+    fill ("#F7F1DA");
+    stroke("#917A4F")
+    strokeWeight(1);
+    arc(320, 236, 40, 20,0, PI);
+    
+    
+   
     
     
 
     	
    // mouth
     noStroke()
+    fill ("black")
     arc(mouthShadowX1, mouthShadowY1, mouthShadowX2, mouthShadowY2, 0, PI);
     stroke("black")
     strokeWeight(5)
     fill("red")
     arc(mouthRedX1, mouthRedY1, mouthRedX2, mouthRedY2, 0, PI);
-  
+   
+    mouthShadowX1 = map(mouseX, 0, width, 280, 305);
+
     
     
    
     // teeth
+    
+    
     fill ("white")
     arc(teethX1,teethY1, teethX2, teethY2, 0, PI);
     
@@ -187,6 +209,12 @@ function draw() {
         
        teethY1 = 310;
     }
+ // mustache
+    noStroke()
+   fill("#917A4F");
+   
+	
+    
 
    
    
@@ -199,10 +227,58 @@ function draw() {
     
     
     
-      // whites eyes
+    // whites eyes
+    
     fill ("white")
-     ellipse (leftWhitesEyesX1, leftWhitesEyesY1, leftWhitesEyesX2, WhiteEyeSize);// left
+    stroke("black")
+    strokeWeight(.5)
+    ellipse (leftWhitesEyesX1, leftWhitesEyesY1, leftWhitesEyesX2, WhiteEyeSize);// left
     ellipse (rightWhitesEyesX1, rightWhitesEyesY1, rightWhitesEyesX2, WhiteEyeSize); // right
+    
+     // BlueEyes
+    
+    noStroke();
+    fill ("#78A1BB");
+    var r = map(mouseX, 0, width, 0, 255); // red 
+    var g = map(mouseY,0, height, 0,0); // green
+	var b = map(mouseX, 0, width, 225, 0); // blue
+
+	fill(r, g, b);
+    var x = width / 2;
+	var y = height / 2;
+	var s = width / 2;
+    
+     ellipse(rightEyeX1, rightEyeY1, eyeSize);// right
+    ellipse(leftEyeX1,leftEyeY1, eyeSize);// left
+    
+     
+    // eyebrows
+    
+    noStroke();
+    fill ("#917A4F")
+    
+   
+	
+    ellipse (eyebrowsRightX1, eyebrowsRightY1, eyebrowsRightX2, eyebrowsRightY2);
+    ellipse (eyebrowsleftX1, eyebrowsleftY1, eyebrowsleftX2, eyebrowsleftY2);
+    
+    //pupils
+    
+    fill ("black")
+    
+    pupilsRightX1 = map(mouseX,0, width, 425, 455);
+    pupilsLeftX1 = map(mouseX,0, width, 152, 180);
+    
+    ellipse(pupilsRightX1, pupilsRightY1, pupilsRightX2, pupilsRightY2);// right
+	ellipse(pupilsLeftX1, pupilsLeftY1, pupilsLeftX2, pupilsLeftY2);// left
+    
+    
+  
+    
+
+   
+}
+
     
     
    
@@ -213,69 +289,4 @@ function draw() {
     
   
     
-    // BlueEyes
-    noStroke();
-    fill ("#78A1BB");
-
-
-    
-    //gives the speed of 2 to leftEyeX1 and leftEyeY1
-    leftEyeX1 = leftEyeX1 + 2 ;
-
-    
-    // if X is greater than 180....stay at 180
-    if (leftEyeX1 > 180) {
-        
-        leftEyeX1 = 180;
-    }
-
-	
-
-    
-    ellipse(rightEyeX1, rightEyeY1, eyeSize);// right
-    ellipse(leftEyeX1,leftEyeY1, eyeSize);// left
-    
-     // eyebrows
-    noStroke();
-    fill ("#917A4F");
-    
    
-	
-    ellipse (eyebrowsRightX1, eyebrowsRightY1, eyebrowsRightX2, eyebrowsRightY2);
-    ellipse (eyebrowsleftX1, eyebrowsleftY1, eyebrowsleftX2, eyebrowsleftY2);
-    
-       fill ("black")
-     			
-    
-     //pupils
-    
-     pupilsRightX1 = map(mouseX,0, width, 10, 120);
-     
-    
-     
-    //gives the speed of 2 to pupilsLeftX1 
-    pupilsLeftX1 = pupilsLeftX1 + 1.5 ;
-//    
-    
-    // if pupilsLeftX1 is greater than 440....stay at 440
-    if (pupilsLeftX1 > 460) {
-        
-       pupilsLeftX1 = 440;
-    }
-
-
-    ellipse(pupilsRightX1, pupilsRightY1, pupilsRightX2, pupilsRightY2);// right
-	ellipse(pupilsLeftX1, pupilsLeftY1, pupilsLeftX2, pupilsLeftY2);// left
-    
-
-      
-   
-    
-   
-}
-
-function mousePressed() {
-    
-   
-}
-
