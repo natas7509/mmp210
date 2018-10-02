cf35db7f54c8caf9f242909ba7f41300b2000c03
Jon Darling <jon.darling@stu.bmcc.cuny.edu>
2018-10-02 15:21:16 -0400

    creating meme v 0

---
+++ meme_0/sketch.js
@@ -0,0 +1,22 @@
+/*
+Meme v 0
+by Jon
+10.2.2018
+rough draft
+mmp 210 week 5 
+
+*/
+
+function setup() {
+    
+    createcanvas(500, 500);
+}
+
+function draw() {
+    background(0);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    textFont("Comic Sans MS");
+    text("MEME", width/2, height/2);
+}
