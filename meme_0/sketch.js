ce360982891c153f1ba647cba50f19f39b8e694b
Jon Darling <jon.darling@stu.bmcc.cuny.edu>
2018-10-03 12:41:05 -0400

    Added text and pic to memeV_0

--- meme_0/sketch.js
+++ meme_0/sketch.js
@@ -7,16 +7,30 @@ mmp 210 week 5
 
 */
 
-function setup() {
-    
-    createCanvas (500, 500);
+var img;
+
+function preload() {
+	img = loadImage("SP-drunk-img.jpg");
 }
 
-function draw() {
-    background(220);
+
+function setup() {
+    
+    createCanvas (550, 550);
+}
+function draw(){
+	
+
+
+    background(233);
     fill(255);
+   
+    image(img, -300, 0);
     textAlign(CENTER, CENTER);
-    textSize(100);
-    textFont("Comic Sans MS");
-    text("Hello", width/2, height/2);
+    textSize(40);
+    textFont("Trebuchet MS");
+    fill('white');
+    text("Hi problem,", width/2, height/20);
+    fill('red');
+    text(" what seems to be the officer?", width/2, 520);
 }
